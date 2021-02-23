71a0037f6460b993881f95ba993ca8f6d394e794
AndrijanaRisanovic <75060744+AndrijanaRisanovic@users.noreply.github.com>
2021-02-23 22:54:26 +0100

    App

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
